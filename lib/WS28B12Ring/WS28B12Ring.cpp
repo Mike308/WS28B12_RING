6313e7cfe20082208aa8329d6f3b14d13b6ecf5e
Mikołaj Werner <m.werner9205@gmail.com>
2018-06-17 13:08:15 +0200

    - implemented method for set hsv color and changed platformio.ini

---
+++ lib/WS28B12Ring/WS28B12Ring.cpp
@@ -0,0 +1,56 @@
+#include "WS28B12Ring.h"
+
+void WS28B12Ring::setHSVColor(uint16_t h, uint8_t s, uint8_t v, uint8_t *r, uint8_t *g, uint8_t *b) {
+
+  uint8_t diff;
+  uint8_t red, green, blue;
+
+  if (h > 359)
+    h = 359;
+  if (s > 100)
+    s = 100;
+  if (v > 100)
+    v = 100;
+
+  if (h < 61) {
+    red = 255;
+    green = (425 * h) / 100;
+    blue = 0;
+  } else if (h < 121) {
+    red = 255 - ((425 * (h - 60)) / 100);
+    green = 255;
+    blue = 0;
+  } else if (h < 181) {
+    red = 0;
+    green = 255;
+    blue = (425 * (h - 120)) / 100;
+  } else if (h < 241) {
+    red = 0;
+    green = 255 - ((425 * (h - 180)) / 100);
+    blue = 255;
+  } else if (h < 301) {
+    red = (425 * (h - 240)) / 100;
+    green = 0;
+    blue = 255;
+  } else {
+    red = 255;
+    green = 0;
+    blue = 255 - ((425 * (h - 300)) / 100);
+  }
+
+  s = 100 - s;
+  diff = ((255 - red) * s) / 100;
+  red = red + diff;
+  diff = ((255 - green) * s) / 100;
+  green = green + diff;
+  diff = ((255 - blue) * s) / 100;
+  blue = blue + diff;
+
+  red = (red * v) / 100;
+  green = (green * v) / 100;
+  blue = (blue * v) / 100;
+  *r = red;
+  *g = green;
+  *b = blue;
+}
+
